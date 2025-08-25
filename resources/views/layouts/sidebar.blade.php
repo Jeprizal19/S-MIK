43a597394d6a4e8f3a47d7fe2a189b0cdfdb2021
Jeprizal19 <jjeprizal@gmail.com>
2025-08-25 18:45:45 +0700

    View-sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="flex flex-col text-gray-800 bg-white shadow-sm transition-all duration-300 ease-in-out flex-shrink-0" :class="isSidebarExpanded ? 'w-64' : 'w-20'">
-  <a href="#" class="h-20 flex items-center px-4 bg-gray-800 hover:text-gray-900 hover:bg-gray-500 focus:outline-none focus:text-gray-900 focus:bg-gray-200 overflow-hidden">
+  <a href="#" class="h-20 flex items-center px-4 bg-gradient-to-r from-cyan-700 to-blue-700 hover:text-gray-900 hover:opacity-90 focus:outline-none focus:text-gray-900 focus:bg-gray-200 overflow-hidden">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-12 w-12 text-white flex-shrink-0">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
     </svg>
